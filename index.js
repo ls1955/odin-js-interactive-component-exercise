30b8ba623f4ed7afa0b2276e071398e4825170b7
ls1955 <xxlsxx1955@gmail.com>
2023-09-18 20:20:55 +0800

    Include addMobileMenuEvent function

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let btnOne = document.querySelector(".button-one");
-let listOne = document.querySelector(".list-one");
-let btnTwo = document.querySelector(".button-two");
-let listTwo = document.querySelector(".list-two");
-
-addDropDownEvent(btnOne, listOne);
-addDropDownEvent(btnTwo, listTwo);
-
 function addDropDownEvent(trigger, list) {
     trigger.addEventListener("mouseover", () => {
         list.style.display = "block";
@@ -17,3 +9,27 @@ function addDropDownEvent(trigger, list) {
         trigger.textContent = "Show drop-down";
     });
 }
+
+function addMobileMenuEvent(triggers, contents, contentContainer) {
+    triggers.forEach((trigger, i) => {
+        trigger.addEventListener("click", () => {
+            contentContainer.textContent = contents[i].textContent;
+        });
+    });
+
+    triggers[0].click();
+}
+
+let btnOne = document.querySelector(".button-one");
+let listOne = document.querySelector(".list-one");
+let btnTwo = document.querySelector(".button-two");
+let listTwo = document.querySelector(".list-two");
+
+addDropDownEvent(btnOne, listOne);
+addDropDownEvent(btnTwo, listTwo);
+
+let phoneBtns = document.querySelectorAll(".phone-button");
+let phoneContents = document.querySelectorAll(".phone-button-content");
+let phoneContentContainer = document.querySelector(".phone-content-container");
+
+addMobileMenuEvent(phoneBtns, phoneContents, phoneContentContainer);
