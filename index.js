01d627d25f3750c911ca693cc16c6da291f9305c
ls1955 <xxlsxx1955@gmail.com>
2023-09-20 08:46:21 +0800

    Format code via Prettier

--- index.js
+++ index.js
@@ -1,83 +1,89 @@
 function addDropDownEvent(trigger, list) {
-    trigger.addEventListener("mouseover", () => {
-        list.style.display = "block";
-        trigger.textContent = "Hide drop-down";
-    });
+  trigger.addEventListener("mouseover", () => {
+    list.style.display = "block";
+    trigger.textContent = "Hide drop-down";
+  });
 
-    trigger.addEventListener("mouseleave", () => {
-        list.style.display = "none";
-        trigger.textContent = "Show drop-down";
-    });
+  trigger.addEventListener("mouseleave", () => {
+    list.style.display = "none";
+    trigger.textContent = "Show drop-down";
+  });
 }
 
 function addMobileMenuEvent(triggers, contents, contentContainer) {
-    triggers.forEach((trigger, i) => {
-        trigger.addEventListener("click", () => {
-            contentContainer.textContent = contents[i].textContent;
+  triggers.forEach((trigger, i) => {
+    trigger.addEventListener("click", () => {
+      contentContainer.textContent = contents[i].textContent;
 
-            triggers.forEach(trigger => trigger.classList.remove("highlight"));
-            trigger.classList.add("highlight");
-        });
+      triggers.forEach((trigger) => trigger.classList.remove("highlight"));
+      trigger.classList.add("highlight");
     });
+  });
 
-    triggers[0].click();
+  triggers[0].click();
 }
 
-function addImagesCarouselEvent(imagesContainer, images, prevBtn, nextBtn, navDots) {
-    let index = 0;
-    // Assuming the frame we are working are squares that stick with each
-    // other without any gap.
-    let offset = images[0].width;
-    let fiveSecondsAutoClickId = setInterval(() => {
-        nextBtn.click();
+function addImagesCarouselEvent(
+  imagesContainer,
+  images,
+  prevBtn,
+  nextBtn,
+  navDots
+) {
+  let index = 0;
+  // Assuming the frame we are working are squares that stick with each
+  // other without any gap.
+  let offset = images[0].width;
+  let fiveSecondsAutoClickId = setInterval(() => {
+    nextBtn.click();
+  }, 5000);
+
+  // Generate all those nav dot at here?
+
+  prevBtn.addEventListener("click", () => {
+    index--;
+
+    if (index < 0) index += images.length;
+
+    imagesContainer.style.left = `-${index * offset}px`;
+
+    resetAutoClickInterval();
+    applyHighlight(navDots, index);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    index = (index + 1) % images.length;
+
+    imagesContainer.style.left = `-${index * offset}px`;
+
+    resetAutoClickInterval();
+    applyHighlight(navDots, index);
+  });
+
+  navDots.forEach((navDot, i) => {
+    navDot.addEventListener("click", () => {
+      index = i;
+
+      imagesContainer.style.left = `-${index * offset}px`;
+
+      resetAutoClickInterval();
+      applyHighlight(navDots, index);
+    });
+  });
+
+  function resetAutoClickInterval() {
+    clearInterval(fiveSecondsAutoClickId);
+    fiveSecondsAutoClickId = setInterval(() => {
+      nextBtn.click();
     }, 5000);
+  }
 
-    // Generate all those nav dot at here?
+  function applyHighlight(navDots, index) {
+    navDots.forEach((navDot) => navDot.classList.remove("highlight"));
+    navDots[index].classList.add("highlight");
+  }
 
-    prevBtn.addEventListener("click", () => {
-        index--;
-
-        if (index < 0) index += images.length;
-
-        imagesContainer.style.left = `-${index * offset}px`;
-
-        resetAutoClickInterval();
-        applyHighlight(navDots, index);
-    });
-
-    nextBtn.addEventListener("click", () => {
-        index = (index + 1) % images.length;
-
-        imagesContainer.style.left = `-${index * offset}px`;
-
-        resetAutoClickInterval();
-        applyHighlight(navDots, index);
-    });
-
-    navDots.forEach((navDot, i) => {
-        navDot.addEventListener("click", () => {
-            index = i;
-            
-            imagesContainer.style.left = `-${index * offset}px`;
-
-            resetAutoClickInterval();
-            applyHighlight(navDots, index);
-        });
-    });
-
-    function resetAutoClickInterval() {
-        clearInterval(fiveSecondsAutoClickId);
-            fiveSecondsAutoClickId = setInterval(() => {
-                nextBtn.click();
-        }, 5000);
-    }
-
-    function applyHighlight(navDots, index) {
-        navDots.forEach(navDot => navDot.classList.remove("highlight"));
-        navDots[index].classList.add("highlight");
-    }
-
-    applyHighlight(navDots, 0);
+  applyHighlight(navDots, 0);
 }
 
 let btnOne = document.querySelector(".button-one");
@@ -100,4 +106,4 @@ let prevBtn = document.querySelector(".prev-frame-button");
 let nextBtn = document.querySelector(".next-frame-button");
 let navDots = document.querySelectorAll(".nav-dot");
 
-addImagesCarouselEvent(imagesContainer, images, prevBtn, nextBtn,navDots);
+addImagesCarouselEvent(imagesContainer, images, prevBtn, nextBtn, navDots);
